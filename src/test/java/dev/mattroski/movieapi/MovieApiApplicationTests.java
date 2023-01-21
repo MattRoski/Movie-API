464edd4a50926a63a072711bf7bc5dcf8f955cb6
mattroski <mattroski88@gmail.com>
2023-01-21 13:46:29 -0600

    First commit

---
+++ src/test/java/dev/mattroski/movieapi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mattroski.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
