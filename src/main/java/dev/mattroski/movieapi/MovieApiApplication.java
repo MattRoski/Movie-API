dfcfd26e98989205c2f019910c12787af41987de
mattroski <mattroski88@gmail.com>
2023-01-21 14:24:07 -0600

    connected to db, dependency for env

--- src/main/java/dev/mattroski/movieapi/MovieApiApplication.java
+++ src/main/java/dev/mattroski/movieapi/MovieApiApplication.java
@@ -2,12 +2,24 @@ package dev.mattroski.movieapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
 	}
 
+
 }
+
+//annotations in java are used to let the compiler know what this class does
+//tomcat is the web server for the app, port 8080
+//whitelabel error page, app working but no endpoint to hit
+//Will divide that app in separate layers: Service layer, Data Access Layer, API Layer
+
+//IDE could not resolve @RestController, until I wrote @GetMapping("")
+  //import org.springframework.web.bind.annotation.RestController;
